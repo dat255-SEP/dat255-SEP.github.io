bda8a358f17473ac314c92e1463c44801adfdba5
emilundg <lundgrenemil@hotmail.com>
2017-04-21 13:54:19 +0200

    added eslint

--- tugboat/index.js
+++ tugboat/index.js
@@ -3,9 +3,9 @@ var express = require('express')
 var expressVue = require('express-vue')
 var app = express();
 
-app.engine('vue', expressVue);
-app.set('view engine', 'vue');
-app.set('views', path.join(__dirname, '/views'));
+app.engine('vue', expressVue)
+app.set('view engine', 'vue')
+app.set('views', path.join(__dirname, '/views'))
 app.set('vue', {
     componentsDir: path.join(__dirname, '/views/components'),
     defaultLayout: 'layout'
