4a84d1bff31bab8de54cdadb36ca2dbbd7b3ab41
Seblerr <sibbe-95@hotmail.com>
2017-04-25 14:37:50 +0200

    Removed stuff

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express')
 var expressVue = require('express-vue')
 var app = express()
 var index = require('./routes/index')
-var livereload = require('express-livereload')
+
 
 app.engine('vue', expressVue)
 app.set('view engine', 'vue')
@@ -17,5 +17,3 @@ app.use('/', index);
 
 app.listen(3000)
 console.log('Express server listening on port 3000')
-
-module.exports = app;
