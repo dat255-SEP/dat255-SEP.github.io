45686c4b749832aa76333c6cc6673d39f9c68a01
emilundg <lundgrenemil@hotmail.com>
2017-04-25 23:21:12 +0200

    fixed GET request for state_update

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 var querystring = require('querystring')
 const api = axios.create({timeout: 5000})
 
-export async function test() {
+export async function test2() {
     api.post('http://192.168.56.101:8080/mb/mqs',
     querystring.stringify({
 
@@ -15,7 +15,23 @@ export async function test() {
             'Content-Type': 'application/json'
         }
     }).then(function(response) {
-        console.log(response.data)
+        console.log(Promise.resolve(response.data))
+    }).catch((error) => {
+      console.log(error)
+    })
+}
+
+
+export async function test() {
+    api.get('http://192.168.56.101:8080/dmp/mss/state_update?count=30&api_key=mb%2Fmqs',
+    {
+        headers: {
+            'X-PortCDM-UserId': 'porter',
+            'X-PortCDM-Password': 'porter',
+            'X-PortCDM-APIKey': 'eeee'
+        }
+    }).then(function(response) {
+        console.log(Promise.resolve(response.data))
     }).catch((error) => {
       console.log(error)
     })
