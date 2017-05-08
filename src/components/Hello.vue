c1076146c7378564808b1e8524b9e76185cb0d8c
LukaMrk <lukamrkonjic163@gmail.com>
2017-05-08 19:38:38 +0200

    Makes the website look more crisp

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -7,11 +7,15 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 </head>
 
-<button type="button" class="btn btn-default">Bootstrap Test</button>
+<!-- <button type="button" class="btn btn-default">Bootstrap Test</button> -->
 
-  <h1>{{ msg }}</h1>
+  <!-- <h1>{{ msg }}</h1> -->
+  <div class="hero">
+    <img class="logo" src="../assets/tuglife-logo.png" width="30%">
+    <br>
+    <button type="button" class="btn btn-book">Book a Tug boat</button>
+  </div>
 
-  <img src="../assets/tommy.png" width="30%">
   <table class="table">
     <tr>
       <td>
@@ -176,6 +180,8 @@ export default {
 
           this.boatArray = filteredTugs
 
+          console.log(filteredTugs)
+
           const betweenStates = filteredTugs.map(s => (s.between))
 
           const toFromArray = betweenStates.filter(function (el) {
@@ -215,6 +221,41 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+body {
+  overflow-x: hidden;
+  font-family: "Roboto Slab", "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+.logo {
+
+}
+
+.btn-book {
+  color: white;
+  background-color: #3498db;
+  border-color: #3498db;
+  font-family: "Montserrat", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  text-transform: uppercase;
+  font-weight: 700;
+}
+.btn-book:hover,
+.btn-book:focus,
+.btn-book:active,
+.btn-book.active,
+.open .dropdown-toggle.btn-book {
+  color: white;
+  background-color: #2980b9;
+  border-color: #2980b9;
+}
+
+.hero {
+  background-image: linear-gradient(rgba(0,0,0,0.0), rgba(0,0,0,0.0)), url("../assets/hero-image.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  padding: 40px;
+  background-attachment: fixed;
+}
+
 h1,
 h2 {
   font-weight: normal;
@@ -235,7 +276,8 @@ a {
 }
 
 table {
-  margin: auto;
+  margin: 50px auto;
+
 }
 
 #post_button {
