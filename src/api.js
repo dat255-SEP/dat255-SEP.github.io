c42c543570afbc8ce51a52b885ef841da147e3bf
emilundg <lundgrenemil@hotmail.com>
2017-05-15 12:31:37 +0200

    made the api get request work without CORS

--- src/api.js
+++ src/api.js
@@ -3,27 +3,17 @@ import axios from 'axios'
 const api = axios.create({timeout: 5000})
 
 export async function getBoatStuffs () {
-  return api.get('http://192.168.56.101:8080/dmp/mss/state_update?count=30&api_key=mb%2Fmqs', {
-    headers: {
-      'X-PortCDM-UserId': 'porter',
-      'X-PortCDM-Password': 'porter',
-      'X-PortCDM-APIKey': 'eeee'
-    }
-  })
+  const response = await api.get(`/api/getStates`)
+  if (!response) {
+    throw new Error('could not get states')
+  }
+  return response.data
 }
 
 export async function postState (xmlData) {
-  const querystring = require('querystring')
-  return api.post('https://dev.portcdm.eu/dmp/mss/state_update', querystring.stringify({}), {
-    data: {
-      xmlData
-    },
-    headers: {
-      'X-PortCDM-UserId': 'porter',
-      'X-PortCDM-Password': 'porter',
-      'X-PortCDM-APIKey': 'eeee',
-      'Content-Type': 'application/xml '
-
-    }
-  })
+  const response = await api.get(`/api/postDat`)
+  if (!response) {
+    throw new Error('could not get states')
+  }
+  return response.data
 }
