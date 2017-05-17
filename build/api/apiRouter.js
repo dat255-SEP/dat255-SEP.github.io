c67622790d6820dab83073b927cdada923e67209
emilundg <lundgrenemil@hotmail.com>
2017-05-17 15:21:21 +0200

    fixed parsing locationstate

--- build/api/apiRouter.js
+++ build/api/apiRouter.js
@@ -15,7 +15,7 @@ router.get('/getStates', async(req, res, next) => {
 })
 
 router.post('/postDat/:xml', async(req, res, next) => {
-  const respXml = convertXmlLocation(req.params.xml)
+  const respXml = convertXmlLocation((req.params.xml).split(','))
   const response = await api.post('http://192.168.56.101:8080/dmp/mss/state_update', respXml, {
     headers: {
       'X-PortCDM-UserId': 'porter',
@@ -32,14 +32,14 @@ router.post('/postDat/:xml', async(req, res, next) => {
 function convertXmlLocation (xmlInput) {
   var xml =
         '<ns2:portCallMessage xmlns:ns2="urn:x-mrn:stm:schema:port-call-message:0.0.16">' +
-        '<ns2:portCallId>urn:x-mrn:stm:portcdm:port_call:SEGOT:e093e8f1-7622-435e-b0cb-73f64c58f5e5</ns2:portCallId>' +
-        '<ns2:vesselId>urn:x-mrn:stm:vessel:IMO:9501368</ns2:vesselId>' +
-        '<ns2:messageId>urn:x-mrn:stm:portcdm:message:0719ab7c-22fb-43a1-a21b-dc36bfd45d32</ns2:messageId>' +
-        '<ns2:reportedBy>VesselAppLocStateView</ns2:reportedBy>' +
+        '<ns2:portCallId>' + xmlInput[0] + '</ns2:portCallId>' +
+        '<ns2:vesselId>' + xmlInput[1] + '</ns2:vesselId>' +
+        '<ns2:messageId>' + xmlInput[2] + '</ns2:messageId>' +
+        '<ns2:reportedBy>' + xmlInput[3] + '</ns2:reportedBy>' +
         '<ns2:locationState>' +
-        '<ns2:referenceObject>VESSEL</ns2:referenceObject>' +
-        '<ns2:time>2017-05-10T18:42:00.000Z</ns2:time>' +
-        '<ns2:timeType>ESTIMATED</ns2:timeType>' +
+        '<ns2:referenceObject>' + xmlInput[4] + '</ns2:referenceObject>' +
+        '<ns2:time>' + xmlInput[5] + 'T18:42:00.000Z</ns2:time>' +
+        '<ns2:timeType>' + xmlInput[6] + '</ns2:timeType>' +
         '<ns2:arrivalLocation>' +
         '<ns2:to>' +
         '<ns2:position>' +
