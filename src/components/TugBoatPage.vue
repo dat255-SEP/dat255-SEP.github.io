cf75a2112a3ea868b83c2aac046d179663e5c146
KevinNordenhog <kevinnordenhog@Kevins-MacBook-Pro.local>
2017-05-23 18:36:10 +0200

    Moved the css code to style.css

--- src/components/TugBoatPage.vue
+++ src/components/TugBoatPage.vue
@@ -4,6 +4,7 @@
   <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<!--    <link rel="stylesheet" type="text/css" href="TugBoatStyle.css"/> -->
   </head>
 
   <div class="hero">
@@ -220,6 +221,7 @@
 <script>
 import * as api from '../api'
 import moment from 'moment'
+require('../stylesheets/style.css')
 
 export default {
   async created () {
@@ -363,173 +365,8 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add "scoped" attribute to limit CSS to this component only ->
 <style scoped>
-body {
-  overflow-x: hidden;
-  font-family: "Roboto Slab", "Helvetica Neue", Helvetica, Arial, sans-serif;
-  margin: 0;
-  padding: 0;
-  background: #e5e5e5;
-}
 
-.logo {}
-
-.btn-book {
-  color: white;
-  background-color: #3498db;
-  border-color: #3498db;
-  font-family: "Montserrat", "Helvetica Neue", Helvetica, Arial, sans-serif;
-  text-transform: uppercase;
-  font-weight: 700;
-}
-
-.btn-book:hover,
-.btn-book:focus,
-.btn-book:active,
-.btn-book.active,
-.open .dropdown-toggle.btn-book {
-  color: white;
-  background-color: #2980b9;
-  border-color: #2980b9;
-}
-
-.hero {
-  background-image: linear-gradient(rgba(0, 0, 0, 0.0), rgba(0, 0, 0, 0.0)), url("../assets/hero-image.jpg");
-  background-size: cover;
-  background-repeat: no-repeat;
-  padding: 40px;
-  background-attachment: fixed;
-}
-
-h1,
-h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-
-table {
-  margin: 50px auto;
-}
-
-.post {
-  margin-top: 50px;
-}
-
-.leftbox {
-  padding-top: 0px;
-  margin: 0px;
-}
-
-.message-form {
-  width: 50%;
-  height: 200px;
-  padding: 12px 20px;
-  box-sizing: border-box;
-  border: 2px solid #ccc;
-  border-radius: 4px;
-  background-color: #f8f8f8;
-  resize: none;
-  margin-bottom: 30px;
-  margin-top: 10px;
-}
-
-.inputTime {
-  width: 23%;
-}
-
-.changeRow {
-  vertical-align: top;
-}
-
-.status-form {
-  text-align: center;
-  margin-top: 10px;
-  width: 50%;
-  height: 50px;
-  padding: 12px 20px;
-  box-sizing: border-box;
-  border: 2px solid #ccc;
-  border-radius: 4px;
-  background-color: #f8f8f8;
-  resize: none;
-  margin-bottom: 30px;
-}
-
-.table-container {}
-
-.form-box {
-  margin-bottom: 50px;
-}
-
-.post-container {
-  background-color: #fcfcfc;
-  padding: 20px;
-}
-
-.leftcell {
-  padding-top: 0px
-}
-
-#post_button {
-  height: 50px;
-  width: 20%;
-  border: none;
-  box-shadow: 2px 2px 10px #888888;
-  border-radius: 5px;
-  background-color: rgb(13, 155, 255);
-  transition: all .2s ease-in-out;
-  font-size: 20px;
-  color: rgb(46, 46, 46);
-}
-
-#post_button:hover {
-  transform: scale(1.02);
-}
-
-[type="date"] {
-  background: #fff url(https://cdn1.iconfinder.com/data/icons/cc_mono_icon_set/blacks/16x16/calendar_2.png) 97% 50% no-repeat;
-}
-
-[type="date"]::-webkit-inner-spin-button {
-  display: none;
-}
-
-[type="date"]::-webkit-calendar-picker-indicator {
-  opacity: 0;
-}
-
-label {
-  display: block;
-}
-
-input {
-  border: 1px solid #c4c4c4;
-  border-radius: 5px;
-  background-color: #fff;
-  padding: 3px 5px;
-  box-shadow: inset 0 3px 6px rgba(0, 0, 0, 0.1);
-  width: 190px;
-}
-
-.table-titles th {
-  text-align: center;
-}
-
-.table2-titles th {
-  text-align: center;
-}
 </style>
+-->
