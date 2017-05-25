357000239c2a8f9e52148d4bca738d1088295aec
Seblerr <sibbe-95@hotmail.com>
2017-05-25 14:52:57 +0200

    Fixed time update in service state table and removed ship agent page from router

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import StartPage from '@/components/StartPage'
 import TugBoatPage from '@/components/TugBoatPage'
-import ShipAgentPage from '@/components/ShipAgentPage'
 
 Vue.use(Router)
 
@@ -17,11 +16,6 @@ export default new Router({
       path: '/TugBoatPage',
       name: 'TugBoatPage',
       component: TugBoatPage
-    },
-    {
-      path: '/ShipAgentPage',
-      name: 'ShipAgentPage',
-      component: ShipAgentPage
     }
   ]
 })
